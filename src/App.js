793c2a778e3440c92466a8845e5f1c91ed1f43cc
pragnakasthruri <kpragna25@gmail.com>
2022-04-26 16:44:45 +0530

    Adding the code for developing the complete BL page

--- src/App.js
+++ src/App.js
@@ -29,17 +29,31 @@ class App extends React.Component {
 
   render() {
     return (
-      <>
-        <div>
+        <div class = 'page-content'>
           <h1>Hello {this.state.userName} from BridgeLabz</h1>
           <img src={logo} onClick={this.onClick} 
             alt="BridgeLabz Logo"/>
-        </div>
         <div>
           <input onChange={this.onNameChange} />
           <span className='error-output'>{this.state.nameError}</span>
         </div>
-      </>
+        <p>At BridgeLabz, we are a tech community of</p>
+        <ul>
+            <li>Technologists</li>
+            <li>Thinkers</li>
+            <li>Builders</li>
+        </ul>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+        <p>
+            To know about us, visit <a href = {this.url} onClick={this.onClick}>BridgeLabz </a> 
+            to learn even more about our misiion i.e. <strong>Employability to all</strong>.
+        </p>
+        </div>  
     )
   }
 } 
