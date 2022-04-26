eb70007156f839434b9a6a741614ef070228ef91
pragnakasthruri <kpragna25@gmail.com>
2022-04-26 15:41:04 +0530

    Adding the code for onChange event binding

--- src/App.js
+++ src/App.js
@@ -7,21 +7,30 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      title : 'Hello from Bridgelabz'
-    }
+      userName : ""   }
   }
   //onClick function
   onClick = ($event) => {
     console.log("save button is clicked", $event);
     window.open(this.url, "_blank");
   }
+
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    this.setState({userName: event.target.value})
+  }
   render() {
     return (
+      <>
       <div>
-      <h1>{this.state.title}</h1>
+      <h1>Hello {this.state.userName} from BridgeLabz</h1>
       <img src = {logo} onClick = {this.onClick}
        alt= "The BridgeLabz logo"/>
       </div>
+      <div>
+        <input onChange = {this.onNameChange}/>
+      </div>
+      </>
     );
   }
 }
