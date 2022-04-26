d34dcaa6984e261e216643e84091d98a6cccdfcf
pragnakasthruri <kpragna25@gmail.com>
2022-04-26 16:22:34 +0530

    Adding the code for validating user input

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
+import logo from './assets/img/BL-Logo.png';
 import React from 'react';
 import './App.css';
-import logo from "./assets/img/BL-Logo.png";
 
 class App extends React.Component {
-  url = 'https://www.bridgelabz.com/' 
+  url="https://www.bridgelabz.com/"
   constructor() {
-    super();
+    super() 
     this.state = {
-      userName : ''
+      userName: '',
+      nameError: ''
     }
   }
-  //onClick function
+
   onClick = ($event) => {
-    console.log("save button is clicked", $event);
-    window.open(this.url, "_blank");
+    window.open(this.url,"_blank");
   }
 
+  
   onNameChange = (event) => {
-    console.log("value is ", event.target.value);
-    this.setState({userName: event.target.value})
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    this.setState({ userName: event.target.value})
+    if(nameRegex.test(event.target.value)) {
+      this.setState({nameError: ''})
+    } else {
+      this.setState({nameError: 'Name is Invalid'})
+    }
   }
-    
+
   render() {
     return (
       <>
-      <div>
-      <h1>Hello {this.state.userName} from BridgeLabz</h1>
-      <img src = {logo} onClick = {this.onClick}
-       alt= "The BridgeLabz logo"/>
-      </div>
-      <div>
-        <input onChange = {this.onNameChange}/>
-      </div>
+        <div>
+          <h1>Hello {this.state.userName} from BridgeLabz</h1>
+          <img src={logo} onClick={this.onClick} 
+            alt="BridgeLabz Logo"/>
+        </div>
+        <div>
+          <input onChange={this.onNameChange} />
+          <span className='error-output'>{this.state.nameError}</span>
+        </div>
       </>
-    );
+    )
   }
-}
+} 
 export default App;
-
-
-
