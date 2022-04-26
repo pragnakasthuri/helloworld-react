6a1b4b1717ee64582883d835392c676e125da981
pragnakasthruri <kpragna25@gmail.com>
2022-04-26 15:26:59 +0530

    Adding the code for onClick event binding

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import logo from "./assets/img/BL-Logo.png";
 
 class App extends React.Component {
+  url = 'https://www.bridgelabz.com/' 
   constructor() {
     super();
     this.state = {
       title : 'Hello from Bridgelabz'
     }
   }
+  //onClick function
+  onClick = ($event) => {
+    console.log("save button is clicked", $event);
+    window.open(this.url, "_blank");
+  }
   render() {
     return (
       <div>
       <h1>{this.state.title}</h1>
-      <img src = {logo}
+      <img src = {logo} onClick = {this.onClick}
        alt= "The BridgeLabz logo"/>
       </div>
     );
